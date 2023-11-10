dd59a2d06158c1793adcd7dd13babbf9207fa0eb
Aksh-007 <akshaybarapatre54@gmail.com>
2023-11-10 14:18:06 +0530

    Fix - react-router added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import Form from "./components/Form.jsx";
 import HeroSection from "./components/HeroSection.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      {/* <Navbar /> */}
-      {/* <HeroSection /> */}
-      {/* <Form /> */}
-
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/guideForm" element={<Form />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
