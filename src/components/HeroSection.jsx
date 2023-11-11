8e01a77a707200f92cb4e7c6cffa9a8fb02876c3
Aksh-007 <akshaybarapatre54@gmail.com>
2023-11-11 23:06:43 +0530

    Feat - card created

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,23 @@
-import { useMediaQuery, useTheme, Box } from "@mui/material";
+import { useMediaQuery, useTheme, Box, Container } from "@mui/material";
 import HeroDesktop from "./body/HeroDesktop.jsx";
 import HeroMobile from "./body/HeroMobile.jsx";
+import Card from "./Card.jsx";
 const HeroSection = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   return (
-    <Box sx={{ bgcolor: "#31E3AE" }}>
-      <Box>{isMobile ? <HeroMobile /> : <HeroDesktop />}</Box>
-    </Box>
+    <>
+      <Box sx={{ bgcolor: "#31E3AE" }}>
+        <Box>{isMobile ? <HeroMobile /> : <HeroDesktop />}</Box>
+      </Box>
+
+      {/* Card Container */}
+      <Box>
+        <Container sx={{ padding: "40px 50px" }}>
+          <Card />
+        </Container>
+      </Box>
+    </>
   );
 };
 
