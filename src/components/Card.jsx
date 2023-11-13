4f48bb6657b9e91da46fb4ce367f0028269ce44b
Aksh-007 <akshaybarapatre54@gmail.com>
2023-11-13 13:05:38 +0530

    Fix - small bug fixes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,9 +19,9 @@ const Card = ({
   return (
     <Box
       sx={{
-        width: isExplored ? "840px" : "350px",
+        // width: isExplored ? "840px" : "350px",
         // width: isExplored ? "840px" : "30%",
-        // width: isExplored ? "840px" : "420px",
+        width: isExplored ? "840px" : "420px",
         borderRadius: "10px",
         boxShadow: "2px 2px 10px 0px rgba(0, 0, 0, 0.10)",
         padding: isExplored ? "0px" : "20px",
