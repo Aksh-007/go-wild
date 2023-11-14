27e38256bfde3c4093b204eef340dec57fdad9b4
Aksh-007 <akshaybarapatre54@gmail.com>
2023-11-14 19:23:09 +0530

    Fix - transition fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,7 +26,7 @@ const Card = ({
         padding: isExplored ? "0px" : "20px",
         display: "flex",
         flexDirection: isExplored ? "row" : "column",
-        transition: "width 0.7s ease-in-out",
+        transition: "width 0.5s ease-in-out",
         // minHeight: isExplored ? "460px" : "100%",
         minHeight: isExplored ? "70%" : "100%",
       }}
